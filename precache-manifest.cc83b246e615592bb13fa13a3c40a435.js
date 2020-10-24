de693abefa38fec6fb062e106d03f8c5b289523c
Bart Dority <b0rgBart3@gmail.com>
2020-10-24 13:57:28 -0700

    Updates

---
+++ precache-manifest.cc83b246e615592bb13fa13a3c40a435.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50ade509387e70b64ec969a0ed53068d",
+    "url": "/name_search/index.html"
+  },
+  {
+    "revision": "fa3a01cac8036934afb8",
+    "url": "/name_search/static/css/main.30b47a9d.chunk.css"
+  },
+  {
+    "revision": "edd58a450d5f8c73aecf",
+    "url": "/name_search/static/js/2.ba0b3281.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/name_search/static/js/2.ba0b3281.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa3a01cac8036934afb8",
+    "url": "/name_search/static/js/main.325b2d56.chunk.js"
+  },
+  {
+    "revision": "abcf1bd7987294456425",
+    "url": "/name_search/static/js/runtime-main.b7a1b1c3.js"
+  },
+  {
+    "revision": "e6b85ea5b2a7d8398cbd1c87207711ea",
+    "url": "/name_search/static/media/asc.e6b85ea5.svg"
+  },
+  {
+    "revision": "b4cfccada5bd451ff2818b556556bc7b",
+    "url": "/name_search/static/media/desc.b4cfccad.svg"
+  },
+  {
+    "revision": "48482b1b6db243b2b75613a49ecee929",
+    "url": "/name_search/static/media/friend1.48482b1b.jpg"
+  },
+  {
+    "revision": "896e429d85abc88fb785ad92cff118a3",
+    "url": "/name_search/static/media/friend2.896e429d.jpg"
+  }
+]);
